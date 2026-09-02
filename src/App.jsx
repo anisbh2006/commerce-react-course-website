876633c6f3f05072a5ff45a70a1e214e592237ff
anis <anis252216@gmail.com>
2026-09-02 15:43:05 +0100

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Home from './pages/Home';
 import Auth from './pages/Auth';
 import Checkout from './pages/Checkout';
 import './App.css'
+import Navbar from './components/Navbar';
 
 function App() {
 
 
   return (
   <div className="app">
+    <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/auth" element={<Auth />} />
