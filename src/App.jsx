195156e3c0b313e51e8e5d707fcb31598161e4f0
anis <anis252216@gmail.com>
2026-09-03 17:52:26 +0100

    complete all

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,29 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
-import ProductDetails from "./pages/ProductDetails";
 import Navbar from "./components/Navbar";
-import AuthProvider from "./context/AuthContext";
+
 import "./App.css";
+import AuthProvider from "./context/AuthContext";
+import ProductDetails from "./pages/ProductDetails";
+import CartProvider from "./context/CartContext";
 
 function App() {
-    return (
-        <AuthProvider>
-            <div className="app">
-                <Navbar />
-
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/auth" element={<Auth />} />
-                    <Route path="/checkout" element={<Checkout />} />
-                    <Route
-                        path="/products/:id"
-                        element={<ProductDetails />}
-                    />
-                </Routes>
-            </div>
-        </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <CartProvider>
+        <div className="app">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+          </Routes>
+        </div>
+      </CartProvider>
+    </AuthProvider>
+  );
 }
 
 export default App;
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,69 +1,60 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useState, useContext } from "react";
 
-export const AuthContext = createContext(null);
+const AuthContext = createContext(null);
 
 export default function AuthProvider({ children }) {
-    const [user, setUser] = useState(() => {
-        const email = localStorage.getItem("currentUserEmail");
+  const [user, setUser] = useState(
+    localStorage.getItem("currentUserEmail")
+      ? { email: localStorage.getItem("currentUserEmail") }
+      : null
+  );
 
-        return email ? { email } : null;
-    });
+  function signUp(email, password) {
+    const users = JSON.parse(localStorage.getItem("users") || "[]");
 
-    function signup(email, password) {
-        const users = JSON.parse(localStorage.getItem("users") || "[]");
-
-        const existingUser = users.find((u) => u.email === email);
-
-        if (existingUser) {
-            throw new Error("User already exists");
-        }
-
-        users.push({
-            email,
-            password,
-        });
-
-        localStorage.setItem("users", JSON.stringify(users));
-        localStorage.setItem("currentUserEmail", email);
-
-        setUser({ email });
+    if (users.find((u) => u.email === email)) {
+      return { success: false, error: "Email already exists" };
     }
+    const newUser = { email, password };
+    users.push(newUser);
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("currentUserEmail", email);
 
-    function login(email, password) {
-        const users = JSON.parse(localStorage.getItem("users") || "[]");
+    setUser({ email });
 
-        const existingUser = users.find(
-            (u) => u.email === email && u.password === password
-        );
+    return { success: true };
+  }
 
-        if (!existingUser) {
-            throw new Error("Invalid email or password");
-        }
-
-        localStorage.setItem("currentUserEmail", email);
-
-        setUser({ email });
-    }
-
-    function logout() {
-        localStorage.removeItem("currentUserEmail");
-        setUser(null);
-    }
-
-    return (
-        <AuthContext.Provider
-            value={{
-                user,
-                signup,
-                login,
-                logout,
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
+  function login(email, password) {
+    const users = JSON.parse(localStorage.getItem("users") || "[]");
+    const user = users.find(
+      (u) => u.email === email && u.password === password
     );
+
+    if (!user) {
+      return { success: false, error: "Invalid email or password" };
+    }
+
+    localStorage.setItem("currentUserEmail", email);
+    setUser({ email });
+
+    return { success: true };
+  }
+
+  function logout() {
+    localStorage.removeItem("currentUserEmail");
+    setUser(null);
+  }
+
+  return (
+    <AuthContext.Provider value={{ signUp, user, logout, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 export function useAuth() {
-    return useContext(AuthContext);
+  const context = useContext(AuthContext);
+
+  return context;
 }
